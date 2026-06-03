1a8e929eedb061cde0b5c3193ed6365fc870c203
hwangsubin318 <2545378@donga.ac.kr>
2026-06-03 21:07:02 +0900

    모델 학습 기능 추가

--- breast_cancer.py
+++ breast_cancer.py
@@ -1,8 +1,17 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 
 data = load_breast_cancer()
 
 x = data.data
 y = data.target
-print(X.shape)
-print(y.shape)
+X_train, X_test, y_train, y_test = train_test_split(
+    x,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+model = DecisionTreeClassifier()
+
+model.fit(X_train, y_train)
